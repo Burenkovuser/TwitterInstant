f9cdbafab31ce4a468e6bf0d7c07214df441d6ac
Burenkovuser <nb2207@yandex.ru>
2017-07-04 11:51:34 +0700

    Requesting Access to Twitter

--- TwitterInstant/RWSearchFormViewController.m
+++ TwitterInstant/RWSearchFormViewController.m
@@ -9,12 +9,28 @@
 #import "RWSearchFormViewController.h"
 #import "RWSearchResultsViewController.h"
 #import <ReactiveCocoa/ReactiveCocoa.h>
+#import "RACEXTScope.h"
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
+
+typedef NS_ENUM(NSInteger, RWTwitterInstantError) {
+    RWTwitterInstantErrorAccessDenied,
+    RWTwitterInstantErrorNoTwitterAccounts,
+    RWTwitterInstantErrorInvalidResponse
+};
+
+static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
+
+
 
 @interface RWSearchFormViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *searchText;
 
 @property (strong, nonatomic) RWSearchResultsViewController *resultsViewController;
+
+@property (strong, nonatomic) ACAccountStore *accountStore;
+@property (strong, nonatomic) ACAccountType *twitterAccountType;
 
 @end
 
@@ -31,15 +47,57 @@
   self.resultsViewController = self.splitViewController.viewControllers[1];
   
     //сигнал если текст соответсует методу isValidSearchText то меняется цвет с желтого на белый
+    @weakify(self)
     [[self.searchText.rac_textSignal
       map:^id(NSString *text) {
           return [self isValidSearchText:text] ?
           [UIColor whiteColor] : [UIColor yellowColor];
       }]
      subscribeNext:^(UIColor *color) {
+         @strongify(self)
          self.searchText.backgroundColor = color;
      }];
+    
+    self.accountStore = [[ACAccountStore alloc] init];
+    self.twitterAccountType = [self.accountStore
+                               accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    //вызываем метод авторизации
+    [[self requestAccessToTwitterSignal]
+     subscribeNext:^(id x) {
+         NSLog(@"Access granted");
+     } error:^(NSError *error) {
+         NSLog(@"An error occurred: %@", error);
+     }];
 }
+
+- (RACSignal *)requestAccessToTwitterSignal {
+    
+    // 1 - define an error
+    NSError *accessError = [NSError errorWithDomain:RWTwitterInstantDomain
+                                               code:RWTwitterInstantErrorAccessDenied
+                                           userInfo:nil];
+    
+    // 2 - create the signal
+    @weakify(self)
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        // 3 - request access to twitter
+        @strongify(self)
+        [self.accountStore
+         requestAccessToAccountsWithType:self.twitterAccountType
+         options:nil
+         completion:^(BOOL granted, NSError *error) {
+             // 4 - handle the response
+             if (!granted) {
+                 [subscriber sendError:accessError];
+             } else {
+                 [subscriber sendNext:nil];
+                 [subscriber sendCompleted];
+             }
+         }];
+        return nil;
+    }];
+}
+
 //проверяем чтобы строка была больше 2 символов
 - (BOOL)isValidSearchText:(NSString *)text {
     return text.length > 2;
