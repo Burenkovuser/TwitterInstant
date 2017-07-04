db1f46c19c218fb69122594cdc4fb0612b142cbd
Burenkovuser <nb2207@yandex.ru>
2017-07-04 12:05:49 +0700

    Searching Twitter

--- TwitterInstant/RWSearchFormViewController.m
+++ TwitterInstant/RWSearchFormViewController.m
@@ -61,15 +61,91 @@ static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
     self.accountStore = [[ACAccountStore alloc] init];
     self.twitterAccountType = [self.accountStore
                                accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
-    //вызываем метод авторизации
-    [[self requestAccessToTwitterSignal]
+    
+    //вызываем метод
+    [[[[[self requestAccessToTwitterSignal]
+        then:^RACSignal *{
+            @strongify(self)
+            return self.searchText.rac_textSignal;
+        }]
+       filter:^BOOL(NSString *text) {
+           @strongify(self)
+           return [self isValidSearchText:text];
+       }]
+      flattenMap:^RACStream *(NSString *text) {
+          @strongify(self)
+          return [self signalForSearchWithText:text];
+      }]
      subscribeNext:^(id x) {
-         NSLog(@"Access granted");
+         NSLog(@"%@", x);
      } error:^(NSError *error) {
          NSLog(@"An error occurred: %@", error);
      }];
 }
+//запрашиваем доступ к поиску
+- (SLRequest *)requestforTwitterSearchWithText:(NSString *)text {
+    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/search/tweets.json"];
+    NSDictionary *params = @{@"q" : text};
+    
+    SLRequest *request =  [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                             requestMethod:SLRequestMethodGET
+                                                       URL:url
+                                                parameters:params];
+    return request;
+}
+//на основе запроса выше создаем сигнал
+- (RACSignal *)signalForSearchWithText:(NSString *)text {
+    
+    // 1 - define the errors
+    NSError *noAccountsError = [NSError errorWithDomain:RWTwitterInstantDomain
+                                                   code:RWTwitterInstantErrorNoTwitterAccounts
+                                               userInfo:nil];
+    
+    NSError *invalidResponseError = [NSError errorWithDomain:RWTwitterInstantDomain
+                                                        code:RWTwitterInstantErrorInvalidResponse
+                                                    userInfo:nil];
+    
+    // 2 - create the signal block
+    @weakify(self)
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        @strongify(self);
+        
+        // 3 - create the request
+        SLRequest *request = [self requestforTwitterSearchWithText:text];
+        
+        // 4 - supply a twitter account
+        NSArray *twitterAccounts = [self.accountStore
+                                    accountsWithAccountType:self.twitterAccountType];
+        if (twitterAccounts.count == 0) {
+            [subscriber sendError:noAccountsError];
+        } else {
+            [request setAccount:[twitterAccounts lastObject]];
+            
+            // 5 - perform the request
+            [request performRequestWithHandler: ^(NSData *responseData,
+                                                  NSHTTPURLResponse *urlResponse, NSError *error) {
+                if (urlResponse.statusCode == 200) {
+                    
+                    // 6 - on success, parse the response
+                    NSDictionary *timelineData =
+                    [NSJSONSerialization JSONObjectWithData:responseData
+                                                    options:NSJSONReadingAllowFragments
+                                                      error:nil];
+                    [subscriber sendNext:timelineData];
+                    [subscriber sendCompleted];
+                }
+                else {
+                    // 7 - send an error on failure
+                    [subscriber sendError:invalidResponseError];
+                }
+            }];
+        }
+        
+        return nil;
+    }];
+}
 
+//запрашивем доступ
 - (RACSignal *)requestAccessToTwitterSignal {
     
     // 1 - define an error
